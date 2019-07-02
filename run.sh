58b9d02f61635b16fbb5cd832d1cb6189379981e
David Whiting <david.whiting@h2o.ai>
2019-07-02 18:16:51 -0500

    updated lots

--- run.sh
+++ run.sh
@@ -17,14 +17,14 @@ fi
 logdir=/log/`date "+%Y%m%d-%H%M%S"`
 mkdir -p "$logdir"
 
-#echo "-------------------------"
-#echo "Welcome to H2O Training
-#echo "-------------------------"
-#echo ""
-#echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
-#echo "- Connect to RStudio on port 8787 (username/password: h2o/h2o)"
+echo "-------------------------"
+echo "Welcome to H2O Training
+echo "-------------------------"
+echo ""
+echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
+echo "- Connect to RStudio on port 8787 (username/password: h2o/h2o)"
 
-source /usr/local/miniconda3/bin/activate h2o
+source /home/h2o/.local/miniconda3/bin/activate h2o
 
 (cd /home/h2o && \
  jupyter --paths >> "$logdir"/jupyter.log && \
