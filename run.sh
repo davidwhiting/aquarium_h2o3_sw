e7eb7f1039f01edce06d00a4578bca913df3eedd
David Whiting <david.whiting@h2o.ai>
2019-07-02 11:57:05 -0500

    Updated end1 h2o only

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ echo ""
 echo "- Connect to Jupyter notebook on port 8888 (password: h2o)"
 echo "- Connect to RStudio on port 8787 (username/password: h2o/h2o)"
 
-source /home/h2o/Miniconda3/bin/activate h2o
+source /usr/local/miniconda3/bin/activate h2o
 
 (cd /home/h2o && \
  jupyter --paths >> "$logdir"/jupyter.log && \
