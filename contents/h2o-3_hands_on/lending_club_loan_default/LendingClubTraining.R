febcae8d000064ec66b68db89c7bda9c21604dba
David Whiting <david.whiting@h2o.ai>
2019-07-11 09:31:11 -0500

    updated first half of R training

--- contents/h2o-3_hands_on/lending_club_loan_default/LendingClubTraining.R
+++ contents/h2o-3_hands_on/lending_club_loan_default/LendingClubTraining.R
@@ -1,114 +1,260 @@
+###
+### Lending Club Loan Default
+###
+
+## In this tutorial, we will go through a step-by-step workflow to determine
+## loan deliquency. Predictions will be made based only on the information
+## available at the time the loan was issued. Our data is a portion of the
+## public Lending Club data set.
+
+### Workflow
+
+##  1. Start H2O-3 cluster
+##  2. Import data
+##  3. Clean data
+##  4. Feature engineering
+##  5. Model training
+##  6. Examine model accuracy
+##  7. Interpret model
+##  8. Save and reuse model
+##  9. AutoML (optional)
+## 10. Stop H2O-3 cluster
+
+
+#################################################################################
+###
+### Step 1 (of 10). Start H2O-3 cluster
+###
+#################################################################################
+
+## The method you use for starting and stopping an H2O-3 cluster will depend on
+## how H2O is set up on your system. The method we show here is the simplest and
+## most straightforward. It is critical, however, that you properly shut down
+## any H2O cluster that you start.
+
 library(h2o)
+h2o.init(bind_to_localhost = FALSE)
 
-# Step 1 (of 10). Start H2O-3 cluster -------------------------------------
+#################################################################################
+###
+### Step 2 (of 10). Import data
+###
+#################################################################################
 
-h2o.init(bind_to_localhost = F)
+## A full description of the complete Lending Club data is available here:
+## https://www.kaggle.com/pragyanbo/a-hitchhiker-s-guide-to-lending-club-loan-data/notebook
+##
+## The data set we use below is a local copy of
+## https://s3-us-west-2.amazonaws.com/h2o-tutorials/data/topics/lending/lending_club/LoanStats3a.csv.
 
-# Step 2 (of 10). Import data ---------------------------------------------
+### View and Inspect Data
+##
+## The loans data is loaded directly into H2O using the following command:
 
-# View Data
-# https://s3-us-west-2.amazonaws.com/h2o-tutorials/data/topics/lending/lending_club/LoanStats3a.csv
-loans <- h2o.importFile("../../data/lending_club/LoanStats3a.csv", 
-                        col.types = c("int_rate" = "string",
-                                      "revol_util" = "string",
-                                      "emp_length" = "string",
-                                      "verification_status" = "string"))
+loans = h2o.importFile("../../data/lending_club/LoanStats3a.csv", 
+                       col.types = c("int_rate" = "string",
+                                     "revol_util" = "string",
+                                     "emp_length" = "string",
+                                     "verification_status" = "string"))
+
+## Note that the h2o.importFile command completely bypassed R and loaded the
+## data directly into H2O memory. Alternatively, one could load a dataset into a
+## dataframe then pass it to H2O. The approach we use above is far more
+## efficient and generally recommended, especially as data size increases. 
+
+### Inspect the Data with H2O Flow
+##
+## Now is a good time to enable H2O Flow. Although H2O Flow can be used for
+## everything from loading data to building models to creating production code,
+## we use it here for data investigation and H2O system monitoring. 
+##
+## Note: the reported IP above
+##
+##     Connecting to H2O server at http://127.0.0.1:54321 ... successful.
+##
+## is the local IP within your particular cloud instance.
+##
+## To open H2O Flow in your own browser, copy your browser URL and replace the
+## port with 54321. 
+##
+## For example, my RStudio URL is
+##
+##     http://52.202.98.125:8787.
+##
+## After opening a new browser tab or window, I copy the address and replace
+## port 8787 with 54321: 
+##
+##    http://52.202.98.125:54321.
+
+## A quick summary of the data size and data fields are shown below.
 
 dim(loans)
-
 head(loans)
 
-# Filter Loans
-# 
-# Now we will filter out loans that are ongoing.  These are loans with loan 
-# status like "Current" and "In Grace Period".
+### Editorial comment: Data preparation for modeling
+##
+## In this tutorial, we either omit or rush through some steps that a modeler
+## normally would spend a considerable amount of time on. For example, we
+## completely skip exploratory data analysis used in conjunction with
+## modeling. Additionally, the process of defining the problem is often
+## iterative and takes a lot of thought and effort. We make the assumption here
+## that this work has already been done by the modeler.
+##
+## In reality, the majority of a modeler's time is spent on problem definition
+## and data cleaning/wrangling/munging. Our speed at going through these steps
+## to demonstrate the use of H2O-3 in no way minimizes the importance of careful
+## and thoughtful data preparation for model building. 
+
+#################################################################################
+###
+### Step 3 (of 10). Clean data
+###
+#################################################################################
+
+## Part 1. Defining the problem and creating the response variable
+##
+## The total number of loans in our data is
 
 num_unfiltered_loans = nrow(loans)
 num_unfiltered_loans
 
+## Because we are interested in loan default, we need to look at the loan_status
+## column. 
+
 head(h2o.table(loans$loan_status), 20)
+
+## Like many real data sources, loan_status is messy and contains multiple
+## (somewhat overlapping) categories. Before modeling, we will need to clean
+## this up by (1) removing loans that are still ongoing, and (2) simplifying the
+## response column. 
+
+## (1) Filter Loans
+## In order to build a valid model, we have to remove loans that are not yet
+## completely "good" or "bad". These ongoing loans have loan_status like
+## "Current" and "In Grace Period". 
 
 ongoing_status = c("Current", 
                    "In Grace Period", 
                    "Late (16-30 days)",
-                   "Late (31-120 days)")
-
+                   "Late (31-120 days)",
+                   "Does not meet the credit policy.  Status:Current",
+                   "Does not meet the credit policy.  Status:In Grace Period")
 loans = loans[!loans$loan_status %in% ongoing_status, ]
+
+## After filtering out ongoing loans, we now have
 
 num_filtered_loans = nrow(loans)
 num_filtered_loans
 
+## loans whose final state is known, which means we filtered out 
+
 num_loans_filtered_out = num_unfiltered_loans - num_filtered_loans
 num_loans_filtered_out
 
-# Create Response Column
-# 
-# Our response column will be called: bad_loan.  The bad_loan column will be 
-# positive if the loan was not completely paid off.
+## loans.
+##
+## These loans are now summarized by loan_status as
 
-loans$bad_loan = loans$loan_status != "Fully Paid"
-loans$bad_loan = as.factor(loans$bad_loan) # convert to enum/factor
+head(h2o.table(loans$loan_status), 20)
+
+## (2) Create Response Column
+## Let's name our response column bad_loan, which will be equal to one if the
+## loan was not completely paid off. 
+
+fully_paid = c("Fully Paid",
+               "Does not meet the credit policy.  Status:Fully Paid"
+               )
+loans$bad_loan = !(loans$loan_status %in% fully_paid)
+
+## Make the bad_loan column a factor so we can build a classification model, 
+
+loans$bad_loan = as.factor(loans$bad_loan) 
+
+## The percentage of bad loans is given by
 
 bad_loan_dist = h2o.table(loans$bad_loan)
 bad_loan_dist$Percentage = h2o.round(100 * bad_loan_dist$Count / nrow(loans))
 bad_loan_dist
-# About one in five loans eventually become bad.
 
-# Step 3 (of 10). Clean data ----------------------------------------------
-#
-# We have multiple columns that are numeric but are being treated as string 
-# because of their syntax. In this section, we will convert these to numeric. 
-# Our machine learning models will have a greater ability to understand numeric 
-# features than strings.
+## Part 2. Convert strings to numeric
+## Consider the data columns int_rate, revol_util, and emp_length:
 
 head(loans[c("int_rate", "revol_util", "emp_length")], 10)
 
-# Convert int_rate to numeric
+## Both int_rate and revol_util are inherently numeric but entered as
+## percentages. Since they include a "%" sign, they are read in as strings. The
+## solution for both of these columns is simple: strip the "%" sign and convert
+## the strings to numeric. 
+##
+## The emp_length column is only slightly more complex. Besides removing the
+## "year" or "years" word, we have to deal with "< 1" and "10+", which aren't
+## directly numeric. If we define "< 1" as 0 and "10+" as 10, then emp_length
+## can also be cast as numeric.
+##
+## We demonstrate the steps for converting these string variables into numeric
+## values below. 
+
+## Convert int_rate
+
 loans$int_rate = h2o.gsub(pattern = "%", replacement = "", x = loans$int_rate) # strip %
-loans$int_rate = h2o.trim(loans$int_rate) # trim ws
-loans$int_rate = as.numeric(loans$int_rate) # change to a numeric
+loans$int_rate = h2o.trim(loans$int_rate) # trim whitespace
+loans$int_rate = as.numeric(loans$int_rate) # change to numeric
 
-head(loans$int_rate, 10)
+## Convert revol_util
 
-# Now that we have converted interest rate to numeric, we can use the hist 
-# function to see the distribution of interest rate for good loans and bad loans.
-
-# Bad Loans
-h2o.hist(loans[loans$bad_loan == "1", "int_rate"])
-
-# Good Loans
-h2o.hist(loans[loans$bad_loan == "0", "int_rate"])
-# The distribution of interest rate is very different for good loans. 
-# This may be a helpful predictor in our model.
-
-# Convert 'revol_util' to numeric
 loans$revol_util = h2o.gsub(pattern = "%", replacement = "", x = loans$revol_util) # strip %
-loans$revol_util = h2o.trim(loans$revol_util) # trim ws
-loans$revol_util = as.numeric(loans$revol_util) #change to a numeric 
+loans$revol_util = h2o.trim(loans$revol_util) # trim whitespace
+loans$revol_util = as.numeric(loans$revol_util) # change to numeric 
 
-# Convert emp_length to numeric
-# 
-# Use gsub to remove " year" and " years" also translate n/a to "" 
+## Convert emp_length
+
+# Use gsub to remove " year" and " years"; also translate n/a to "" 
 loans$emp_length = h2o.gsub(pattern = "([ ]*+[a-zA-Z].*)|(n/a)", replacement = "",
                             x = loans$emp_length) 
+loans$emp_length = h2o.trim(loans$emp_length) # trim whitespace
 
-# Use trim to remove any trailing spaces 
-loans$emp_length = h2o.trim(loans$emp_length)
-
-# Convert emp_length to numeric 
-# Use sub to convert < 1 to 0 years and do the same for 10 + to 10
-# Hint: Be mindful of spaces between characters
 loans$emp_length = h2o.gsub(pattern = "< 1", replacement = "0", x = loans$emp_length)
 loans$emp_length = h2o.gsub(pattern = "10\\\\+", replacement = "10", x = loans$emp_length)
 loans$emp_length = as.numeric(loans$emp_length)
 
+## The converted results for the three former string variables are
+
 head(loans[c("int_rate", "revol_util", "emp_length")], 10)
 
-# We can also clean up the verification status column. There are multiple values
-# that mean verified: 'VERIFIED - income' and 'VERIFIED - income source'. We will 
-# replace these values with 'verified'.
+## Note: Interest rate distributions
+## Now that we have converted interest rate to numeric, we can use the hist
+## function to compare the interest rate distributions for good and bad loans. 
+
+########### FIX THESE PLOTS ###########
+
+# Bad Loans
+h2o.hist(loans[loans$bad_loan == "1", "int_rate"])
+# Good Loans
+h2o.hist(loans[loans$bad_loan == "0", "int_rate"])
+
+#########################################
+
+## As expected, the bad loan distribution contains a higher proportion of high
+## interest rates than the distribution for good loans. Likewise, the good loan
+## distribution contains a higher proportion of low interest rates than that for
+## bad loans. It is a truism to say that interest rate should be a good
+## predictor of default. 
+##
+## Financial institutions typically determine a loan's interest rate based
+## largely on risk and customer demand. If the underwriting rules are any good
+## at all, then we would expect that interest rate would be a strong predictor
+## of default. In fact, we should be surprised if int_rate is not one of the top
+## two or three variables in our model regardless of algorithm.
+
+## Part 3. Clean up messy categorical columns
+##
+## Much as we did with the loan_status column to create our response variable,
+## the verification_status column needs cleaning
 
 head(loans["verification_status"], 10)
+
+## Note that there are multiple values that mean verified: "VERIFIED - income"
+## and "VERIFIED - income source". We will replace these values with "verified",
 
 loans$verification_status = h2o.sub(pattern = "VERIFIED - income source",
                                     replacement = "verified",
@@ -118,10 +264,17 @@ loans$verification_status = h2o.sub(pattern="VERIFIED - income",
                                     x = loans$verification_status)
 loans$verification_status = as.factor(loans$verification_status)
 
+## resulting in
+
 h2o.table(loans$verification_status)
 
-# Step 4 (of 10). Feature engineering -------------------------------------
-# 
+#################################################################################
+###
+### Step 4 (of 10). Feature engineering
+###
+#################################################################################
+
+
 # Now that we have cleaned our data, we can add some new columns to our dataset 
 # that may help improve the performance of our supervised learning models.
 # 
